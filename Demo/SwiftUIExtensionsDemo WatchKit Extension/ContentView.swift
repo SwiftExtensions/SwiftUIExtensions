db8a5b7e1e774b99811ea1aae8a3f4dcb56676cf
Alex <alex.buzov@icloud.com>
2019-12-12 21:36:01 -0800

    watchOS target

---
+++ Demo/SwiftUIExtensionsDemo WatchKit Extension/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIExtensionsDemo WatchKit Extension
+//
+//  Created by Alex on 2019-12-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo/SwiftUIExtensionsDemo WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  SwiftUIExtensionsDemo WatchKit Extension
+//
+//  Created by Alex on 2019-12-12.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
