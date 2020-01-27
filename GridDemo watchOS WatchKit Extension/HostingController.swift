f151f6f6756a70e667bbba65b90f3f229d12b673
Alex <alex.buzov@icloud.com>
2020-01-26 22:09:45 -0800

    watchOS target

---
+++ GridDemo watchOS WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  GridDemo watchOS WatchKit Extension
+//
+//  Created by Alex on 2020-01-26.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ GridDemo watchOS WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  GridDemo watchOS WatchKit Extension
+//
+//  Created by Alex on 2020-01-26.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
