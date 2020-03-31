24a807a34257c2bc678b45b38cb6d773ffb02e2f
Alex <alex.buzov@icloud.com>
2020-03-31 01:18:50 -0700

    cleanup

--- Sources/Grid/Styles/Style/GridStyle.swift
+++ Sources/Grid/Styles/Style/GridStyle.swift
@@ -1,19 +1,7 @@
 import SwiftUI
 
-@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 public protocol GridStyle {
-    /// A `View` representing the body of a `ValueSlider`.
     associatedtype Body : View
-    
-    /// Creates a `View` representing the body of a `ValueSlider`.
-    ///
-    /// - Parameter configuration: The properties of the value slider instance being
-    ///   created.
-    ///
-    /// This method will be called for each instance of `ValueSlider` created within
-    /// a view hierarchy where this style is the current `ValueSliderStyle`.
-    func makeBody(configuration: Self.Configuration) -> Self.Body
-
-    /// The properties of a `ValueSlider` instance being created.
     typealias Configuration = GridStyleConfiguration
+    func makeBody(configuration: Self.Configuration) -> Self.Body
 }
